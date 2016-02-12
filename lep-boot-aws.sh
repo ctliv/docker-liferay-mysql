697ff59d608ceb07bdf44342b45bca616216636e
ctliv <ct.livorno@gmail.com>
2016-02-12 18:30:44 +0100

    Added launch script for AWS EC2 instance

---
+++ lep-boot-aws.sh
@@ -0,0 +1,2 @@
+sudo docker run --name lep-db -p 3306:3306 -e MYSQL_ROOT_PASSWORD=adminpwd -e MYSQL_USER=lportal -e MYSQL_PASSWORD=lportal -e MYSQL_DATABASE=lportal -d mysql:5.7
+sudo docker run --name lep-as -p 80:8080 -p 2222:2222 -p 1099:1099 -p 8999:8999 --link lep-db -e LIFERAY_DEBUG=1 -e VM_HOST=ec2-54-173-3-121.compute-1.amazonaws.com -d ctliv/liferay:6.2
