522e8e52e6050fceacd3ebebc05848a6b6f8f27f
ctliv <ct.livorno@gmail.com>
2016-08-11 12:45:42 +0200

    Bug fixing. Updated liferay version from 6.2 to 7.0

--- lep-boot.sh
+++ lep-boot.sh
@@ -65,6 +65,6 @@ if [ $db -eq 1 ]; then
 fi
 
 if [ $as -eq 1 ]; then
-	docker run --name lep-as -p 80:8080 -p 443:8443 -p 2222:22 -p 1099:1099 -p 8999:8999 --link lep-db -e LIFERAY_DEBUG=1 -v /$(dirname $(readlink -f $0))/deploy-run:/var/liferay/deploy -v /$(dirname $(readlink -f $0))/../rainbow/rainbow-operativo/db:/opt/data -e VM_HOST=${host} -e LIFERAY_NOWIZARD=${nowizard} -d ctliv/liferay:6.2
+	docker run --name lep-as -p 80:8080 -p 443:8443 -p 2222:22 -p 1099:1099 -p 8999:8999 --link lep-db -e LIFERAY_DEBUG=1 -v /$(dirname $(readlink -f $0))/deploy-run:/var/liferay/deploy -v /$(dirname $(readlink -f $0))/../rainbow/rainbow-operativo/db:/opt/data -e VM_HOST=${host} -e LIFERAY_NOWIZARD=${nowizard} -d ctliv/liferay:7.0
 fi
 
